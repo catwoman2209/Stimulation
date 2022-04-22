36d05e4621be7f58269a5692aacc63b223114f17
Christiana Taylor <60984198+catwoman2209@users.noreply.github.com>
2022-04-21 23:17:33 -0500

    Delete Racer.py

--- Racer.py
+++
@@ -1,9 +0,0 @@
-#File for Type Racer game code
-import pygame
-import pygame_gui as pygui
-from pygame_gui.core import ObjectID
-import Stimulation as S
-
-pygame.init()
-
-print("Type Racer game code")
