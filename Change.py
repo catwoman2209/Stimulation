30d89511274af3081a958a48db939e7eaad05ddf
Tanner Kellogg <73614668+perennat@users.noreply.github.com>
2022-04-29 05:20:00 -0500

    Update Change.py

--- Change.py
+++ Change.py
@@ -1,9 +1,8 @@
 #File for Quick Change game code
-import pygame
-import pygame_gui as pygui
-from pygame_gui.core import ObjectID
-import Stimulation as S
+import random
 
-pygame.init()
 
-print("Quick Change game code")
+def get_total():
+    return round(random.uniform(0.99, 199.99), 2)
+def get_payment(total, size):
+    return round(random.uniform(total, total + size), 2)
