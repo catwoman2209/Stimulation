61e4ed5e2434e593f48f12a4f7be68228121c3f4
Tanner Kellogg <73614668+perennat@users.noreply.github.com>
2022-04-30 10:57:18 -0500

    Update Change.py

--- Change.py
+++ Change.py
@@ -9,4 +9,4 @@ import random
 def get_total():
     return round(random.uniform(0.99, 199.99), 2)
 def get_payment(total, size):
-    return round(random.uniform(total, total + size), 2)
+    return int(round(random.uniform(total, total + size), 2))
